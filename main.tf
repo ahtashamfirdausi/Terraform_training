95c8fdee81bed11a2239b696e81f9effd7f988f3
Ahtasham Firdausi <ahtahsamfirdausi99@hotmail.com>
2021-05-10 20:25:36 +0530

    commit-updated

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ variable avail_zone {}
 variable env_prefix {}
 
 resource "aws_vpc" "aws_vpc"{
-    cidr_block = var.vpc_cider_block
+    cidr_block = var.vpc_cidr_block
     tags = {
         Name: "${var.env_prefix}-vpc"
     }
@@ -16,8 +16,8 @@ resource "aws_vpc" "aws_vpc"{
 
 resource "aws_subnet" "myapp-subnet-1"{
     vpc_id = aws_vpc.aws_vpc.id
-    cridr_block = var.subnet_cidr_block
-    avail_zone = var.avail_zone
+    cidr_block = var.subnet_cidr_block
+   availability_zone = var.avail_zone
     tags = {
         Name: "${var.env_prefix}-subnet-1"
     }
