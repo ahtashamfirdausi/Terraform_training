cc4d28a8e435459f84678d6919dc97cfa42c0ae4
Ahtasham Firdausi <ahtahsamfirdausi99@hotmail.com>
2021-05-10 23:39:59 +0530

    aws_security_group

--- main.tf
+++ main.tf
@@ -47,3 +47,21 @@ resource "aws_route_table_association" "a-rtb-subnet" {
     route_table_id = aws_route_table.myapp-route-table.id
   
 }
+# if we for default route table 
+/* resource  "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+     tags ={
+            Name: "${var.env_prefix}-main-rtb"
+        
+  
+}*/
+resource  "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+  
+}
