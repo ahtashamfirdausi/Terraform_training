bfc969c2bcf2054143272f6bcdb64f6f0f600efe
Ahtasham Firdausi <ahtahsamfirdausi99@hotmail.com>
2021-05-10 20:59:15 +0530

    main file is updated

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 
-resource "aws_vpc" "aws_vpc"{
+resource "aws_vpc" "myapp-vpc"{
     cidr_block = var.vpc_cidr_block
     tags = {
         Name: "${var.env_prefix}-vpc"
@@ -15,10 +15,31 @@ resource "aws_vpc" "aws_vpc"{
 }
 
 resource "aws_subnet" "myapp-subnet-1"{
-    vpc_id = aws_vpc.aws_vpc.id
+    vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
    availability_zone = var.avail_zone
     tags = {
         Name: "${var.env_prefix}-subnet-1"
     }
 }
+
+resource "aws_internet_gateway" "myapp-igw"{
+    vpc_id = aws_vpc.myapp-vpc.id
+
+     tags={
+            Name: "${var.env_prefix}-igw"
+        }
+}
+
+resource "aws_route_table" "myapp-route-table"{
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+     tags ={
+            Name: "${var.env_prefix}-rtb"
+        }
+
+}
+
