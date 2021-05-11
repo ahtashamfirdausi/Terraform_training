b09075046909a3b4195936d0229844f1198a8dd9
Ahtasham Firdausi <ahtahsamfirdausi99@hotmail.com>
2021-05-11 14:36:20 +0530

    ingress and egress is added in security group

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
 
 resource "aws_vpc" "myapp-vpc"{
     cidr_block = var.vpc_cidr_block
@@ -63,5 +64,28 @@ resource "aws_route_table_association" "a-rtb-subnet" {
 resource  "aws_security_group" "myapp-sg" {
     name = "myapp-sg"
     vpc_id = aws_vpc.myapp-vpc.id
-  
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+   ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+}
+tags = {
+
+    Name: "${var.env_prefix}-sg"
+}
 }
