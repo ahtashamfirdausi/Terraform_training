34d9c46924c83ef348c5d4e8db638902191059a7
Ahtasham Firdausi <ahtahsamfirdausi99@hotmail.com>
2021-05-11 18:58:27 +0530

    ec2 key-pair

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
+variable instance_type {}
+  
+ 
 
 resource "aws_vpc" "myapp-vpc"{
     cidr_block = var.vpc_cidr_block
@@ -110,7 +113,21 @@ output "aws_ami_id" {
   
 }
 
-/* resource "aws_instance" "myapp-server" {
+ resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
-  
-}*/
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone =  var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = "server-key-pair"
+
+    tags = {
+
+         Name: "${var.env_prefix}-server"
+
+   }
+ }
+ 
