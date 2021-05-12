68a41c70a5398fb4d4d4afa253a5ae3350c41152
Ahtasham Firdausi <ahtahsamfirdausi99@hotmail.com>
2021-05-12 20:41:43 +0530

    modulizing the code

--- main.tf
+++ main.tf
@@ -8,52 +8,12 @@ resource "aws_vpc" "myapp-vpc"{
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1"{
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-   availability_zone = var.avail_zone
-    tags = {
-        Name: "${var.env_prefix}-subnet-1"
-    }
-}
 
-resource "aws_internet_gateway" "myapp-igw"{
-    vpc_id = aws_vpc.myapp-vpc.id
-
-     tags={
-            Name: "${var.env_prefix}-igw"
-        }
-}
-
-resource "aws_route_table" "myapp-route-table"{
-    vpc_id = aws_vpc.myapp-vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-     tags ={
-            Name: "${var.env_prefix}-rtb"
-        }
-
-}
 resource "aws_route_table_association" "a-rtb-subnet" {
     subnet_id = aws_subnet.myapp-subnet-1.id 
     route_table_id = aws_route_table.myapp-route-table.id
   
-}
-# if we for default route table 
-/* resource  "aws_default_route_table" "main-rtb" {
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-     tags ={
-            Name: "${var.env_prefix}-main-rtb"
-        
-  
-}*/
+ }
 # To use default security group we have to simple change the name to default in resource block and in
 # name block we have to skip it.
 
