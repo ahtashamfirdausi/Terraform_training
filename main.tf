df2431674566bc89d57a3f65be8f97c950395b86
Ahtasham Firdausi <ahtahsamfirdausi99@hotmail.com>
2021-05-12 13:23:02 +0530

    main file is added

---
+++ main.tf
@@ -0,0 +1,148 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
+  
+ 
+  
+ 
+
+resource "aws_vpc" "myapp-vpc"{
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
+}
+
+resource "aws_subnet" "myapp-subnet-1"{
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+   availability_zone = var.avail_zone
+    tags = {
+        Name: "${var.env_prefix}-subnet-1"
+    }
+}
+
+resource "aws_internet_gateway" "myapp-igw"{
+    vpc_id = aws_vpc.myapp-vpc.id
+
+     tags={
+            Name: "${var.env_prefix}-igw"
+        }
+}
+
+resource "aws_route_table" "myapp-route-table"{
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+     tags ={
+            Name: "${var.env_prefix}-rtb"
+        }
+
+}
+resource "aws_route_table_association" "a-rtb-subnet" {
+    subnet_id = aws_subnet.myapp-subnet-1.id 
+    route_table_id = aws_route_table.myapp-route-table.id
+  
+}
+# if we for default route table 
+/* resource  "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+     tags ={
+            Name: "${var.env_prefix}-main-rtb"
+        
+  
+}*/
+# To use default security group we have to simple change the name to default in resource block and in
+# name block we have to skip it.
+
+resource  "aws_security_group" "myapp-sg" {  
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+   ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+}
+tags = {
+
+    Name: "${var.env_prefix}-sg"
+}
+}
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*"]
+    }
+    filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+  
+}
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+  
+}
+resource "aws_key_pair" "ssh-key" {
+ key_name = "server-key"
+  public_key =  file(var.public_key_location)                       
+}
+
+ resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone =  var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    tags = {
+
+         Name: "${var.env_prefix}-server"
+
+   }
+ }
+ 
